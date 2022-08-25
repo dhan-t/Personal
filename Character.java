54ab5fc78735a60f2cb478ef3765c17289e21e4f
Dhan Micheal Tamparong <111488321+su1kagg@users.noreply.github.com>
2022-08-25 14:47:18 +0800

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,23 @@
+public class Character {
+    int strength; 
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+
+    public void sayMyStrength() {
+            System.out.println("My power level is" + strength);
+    }
+
+    public void sayMyAgility() {
+        System.out.println("My power speed is" + agility);
+    }
+
+    public void sayMyIntelligence() {
+        System.out.println("My knowledge is" + intelligence);
+    }
+    
+} 
