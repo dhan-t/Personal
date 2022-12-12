8276ff010afcdcaa7a117bef277b50ec56950d6a
Dhan Micheal Tamparong <111488321+su1kagg@users.noreply.github.com>
2022-12-12 23:36:41 +0800

    Update checkerist.java

--- checkerist.java
+++ checkerist.java
@@ -1,63 +1,67 @@
 import java.util.Scanner;
 
-public class checkerist {
+public class Attendance {
 
-	static int[] testarray = {15,30,25,19,30,40};
-	
-	public static void main(String[] args) {
-		
-		showArray();
-		Scanner sc = new Scanner(System.in);	
+    static int[] Array = { 15, 30, 25, 19, 30, 40 };
 
-		//REMOVING PART
-		while (!checkArray()) {
-			System.out.println("Enter numbers to be removed");
-			int num = sc.nextInt();
-			
-			findelement(num);
-			showArray();
-		}
-		
-		System.out.println("Empty");
-		
-		}
-	//lists the elements of the array
-	static void showArray()
-	{
-		for(int i = 0; i<testarray.length; i++) {
-			if(testarray[i] > 0) {
-				System.out.print(testarray[i] + " ");
-			}
-		}
-		System.out.println("");
-	}
-	//checker and remover of array elements
-	static void findelement(int num) {
-		
-		for(int x = 0; x < testarray.length; x++) {
-			
-			if(num == testarray[x]) {
-				testarray[x] = 0;		
-			}	
-		}
+    public static void main(String[] args) {
+        // Prints the showArray method.
+        showArray();
+        Scanner sc = new Scanner(System.in);
+        // Takes input from user for the numbers to be removed.
+        while (!checkArray()) {
+            System.out.println("Enter numbers to be removed: \n");
+            int num = sc.nextInt();
+            // Calls in the different methods.
+            findelement(num);
+            present(num);
+            showArray();
+        }
+        System.out.println("Array has been emptied.");
+        sc.close();
+    }
 
-	}
-	//checks if the condition is true (if array is all 0)
-	static boolean checkArray() {
-		
-		boolean isEmpty = false;
-		
-		for(int x = 0; x < testarray.length; x++) {
-				
-			if(testarray[x] != 0)
-			{
-				isEmpty = false;
-			}
-			else {
-				isEmpty = true;
-			}
-		}
-		
-		return isEmpty;
-	}
+    /// METHODS
+    // Lists the elements of the array
+    static void showArray() {
+        for (int i = 0; i < Array.length; i++) {
+            if (Array[i] > 0) {
+                System.out.print(" (" + Array[i] + ")");
+            }
+        }
+        System.out.println(" ");
+    }
+
+    // Checks if the array is empty.
+    static void findelement(int num) {
+        for (int x = 0; x < Array.length; x++) {
+            if (num == Array[x]) {
+                Array[x] = 0;
+            }
+        }
+    }
+
+    // Checks if the number entered is present in the array.
+    static boolean checkArray() {
+        int checkArray = 0;
+        for (int j = 0; j < Array.length; j++) {
+            if (Array[j] != 0) {
+                continue;
+            } else {
+                checkArray++;
+            }
+        }
+        if (checkArray == 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Checks if the number is present IF not, it asks the user to re-enter.
+    static void present(int num) {
+        if (!(num == 15 || num == 30 || num == 25 || num == 19 || num == 30 || num == 40)) {
+            System.out.println("Error! Please enter a valid number.\n");
+        }
+    }
 }
