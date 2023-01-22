b3e0bec7d8b0fa495e3e417e7fcdc6caf92f686a
Dhan Micheal Tamparong <111488321+su1kagg@users.noreply.github.com>
2023-01-22 19:39:00 +0800

    Create rudeasspythongame.py

---
+++ rudeasspythongame.py
@@ -0,0 +1,18 @@
+#Lets play a guessing game!
+
+print("are you down for some game?")
+
+answer1 = input("yes or no?")
+if answer1 == 'yes':
+    print("good!")
+else: 
+    print("fuck off then")
+    exit()
+
+name = input("state your name:")
+print("hi there" + " " + ( name ) + " " + "pleased to meet you!")
+
+age = input("how old are you?")
+print("so you are" + " " + (age) + " " + "years old huh? that's fucking old man.")
+
+#Created Sep. 7, 2022
